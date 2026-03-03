d5906b35b927910174d7cd3ed9d4eb1a219cd25c
prabha63 <yuviprabha6363@gmail.com>
2026-03-03 23:00:20 +0530

    Fix cookies and CORS for deployed frontend

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -27,14 +27,14 @@ export const setRegister = async (req, res) => {
     await User.create({
       email,
       password: hashedpassword,
-      authorizedType : 'local',
+      authorizedType: "local",
       role,
     });
 
     return res.status(201).json({
       msg: "Registered Successfully",
       data: true,
-      authorizedType : 'local'
+      authorizedType: "local",
     });
   } catch (err) {
     res.status(500).json({ msg: err.message });
@@ -49,12 +49,11 @@ export const Login = async (req, res) => {
     return res.status(404).json({ msg: "User not registered" });
   }
 
-if(user.authorizedType === "google"){
-   return res.status(400).json({
-      msg: "This account uses Google Sign-In"
-   })
-}
-
+  if (user.authorizedType === "google") {
+    return res.status(400).json({
+      msg: "This account uses Google Sign-In",
+    });
+  }
 
   const isMatch = await bcrypt.compare(password, user.password);
   if (!isMatch) {
@@ -65,7 +64,7 @@ if(user.authorizedType === "google"){
       {
         userId: user._id,
         role: user.role,
-        email : user.email,
+        email: user.email,
         authProvider: "local",
       },
       process.env.ACCESS_JWT_SECRET,
@@ -76,7 +75,7 @@ if(user.authorizedType === "google"){
       {
         userId: user._id,
         role: user.role,
-        email : user.email,
+        email: user.email,
         authProvider: "local",
       },
       process.env.REFRESH_JWT_SECRET,
@@ -86,37 +85,34 @@ if(user.authorizedType === "google"){
     res.cookie("accessToken", Accesstoken, {
       signed: true,
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      sameSite: "None",
+      secure: true,
       maxAge: 5 * 60 * 1000, // 15 mins
     });
 
     res.cookie("refreshToken", RefreshToken, {
       signed: true,
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      sameSite: "None",
+      secure: true,
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
-
     res.json({
-      msg : 'Login Success',
-      user : {
-        isLogin : true,
-        _id : user._id,
-        roleData : user.role,
-        email : user.email,
-        authProvider : 'local',
-        loginActivity : Date.now()
-      }
-    })
-
+      msg: "Login Success",
+      user: {
+        isLogin: true,
+        _id: user._id,
+        roleData: user.role,
+        email: user.email,
+        authProvider: "local",
+        loginActivity: Date.now(),
+      },
+    });
   } catch (err) {
     res.status(500).json(err.message);
   }
 };
-
 
 // Check Email Exist
 export const EmailExist = async (req, res) => {
@@ -144,19 +140,18 @@ export const EmailExist = async (req, res) => {
 
     const user = await User.findOne({ email });
 
-    
-    if(!user){
-    return res.json({
-      msg: "Not Exist",
-      emailExist: false,
-    });
+    if (!user) {
+      return res.json({
+        msg: "Not Exist",
+        emailExist: false,
+      });
     }
 
     if (user) {
       return res.json({
         msg: "Exist",
         emailExist: true,
-        roleData : user.role
+        roleData: user.role,
       });
     }
   } catch (err) {
@@ -165,69 +160,68 @@ export const EmailExist = async (req, res) => {
   }
 };
 
-
 // google login
 export const googleAuthLogin = async (req, res) => {
   try {
-    const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID)
-    const { token,Role} = req.body
+    const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+    const { token, Role } = req.body;
 
     const ticket = await client.verifyIdToken({
       idToken: token,
       audience: process.env.GOOGLE_CLIENT_ID,
-    })
+    });
 
-    const payload = ticket.getPayload()
-    const { email, name, picture, sub: googleId } = payload
+    const payload = ticket.getPayload();
+    const { email, name, picture, sub: googleId } = payload;
 
-    let user = await User.findOne({ email })
+    let user = await User.findOne({ email });
 
     if (!user) {
       user = await User.create({
-      email,
-      name,
-      picture,
-      googleId,
-      role: Role,
-      authorizedType : 'google'
-    });
+        email,
+        name,
+        picture,
+        googleId,
+        role: Role,
+        authorizedType: "google",
+      });
     }
 
     const AccessToken = jwt.sign(
       {
-        userId: user._id,       
+        userId: user._id,
         role: user.role,
-        email : user.email,
+        email: user.email,
         authProvider: "google",
       },
       process.env.ACCESS_JWT_SECRET,
-      { expiresIn: "15m" }
-    )
+      { expiresIn: "15m" },
+    );
 
     const RefreshToken = jwt.sign(
       {
-        userId: user._id,       
+        userId: user._id,
         role: user.role,
-        email : user.email,
+        email: user.email,
         authProvider: "google",
       },
       process.env.REFRESH_JWT_SECRET,
-      { expiresIn: "7d" }
-    )
+      { expiresIn: "7d" },
+    );
 
     res.cookie("accessToken", AccessToken, {
       httpOnly: true,
       signed: true,
       sameSite: "strict",
       maxAge: 15 * 60 * 1000,
-    })
+    });
 
     res.cookie("refreshToken", RefreshToken, {
       httpOnly: true,
       signed: true,
       sameSite: "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
-    })
+    });
 
     res.json({
       msg: "Google Login success",
@@ -235,18 +229,17 @@ export const googleAuthLogin = async (req, res) => {
         isLogin: true,
         roleData: user.role,
         email: user.email,
-        _id : user._id,
+        _id: user._id,
         name: user.name,
         picture: user.picture,
-        authprovider : "google",
-        loginActivity : Date.now() 
+        authprovider: "google",
+        loginActivity: Date.now(),
       },
-    })
+    });
   } catch (err) {
-    res.status(500).json({ msg: err.message })
+    res.status(500).json({ msg: err.message });
   }
-}
-
+};
 
 // Refresh Token
 export const RefreshToken = async (req, res) => {
@@ -262,8 +255,8 @@ export const RefreshToken = async (req, res) => {
       {
         userId: decoded.userId,
         role: decoded.role,
-        email : decoded.email,
-        authProvider : decoded.authProvider
+        email: decoded.email,
+        authProvider: decoded.authProvider,
       },
       process.env.ACCESS_JWT_SECRET,
       { expiresIn: "15m" },
@@ -285,97 +278,96 @@ export const RefreshToken = async (req, res) => {
 
 // Forget password
 export const ForgetPassword = async (req, res) => {
-  try{
-  const { email } = req.body;
+  try {
+    const { email } = req.body;
 
-  const user = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-  if (!user) {
-    return res.status(404).json({ msg: "User Not Found" });
-  }
-
-  if(user.authorizedType === "google"){
-   return res.status(400).json({
-      msg: "This account uses Google Sign-In"
-   })
-}
-
-  // crypto token
-  const resetToken = crypto.randomBytes(32).toString("hex");
-
-  user.resetPasswordToken = crypto
-    .createHash("sha256")
-    .update(resetToken)
-    .digest("hex");
-
-  user.resetPasswordExpire = Date.now() + 60 * 60 * 1000; // 1hr
-
-  await user.save();
-
- const reseturl = `${process.env.FRONTEND_URL}/reset-password/${resetToken}`;
-
-
-  // send Mail
-  const transporter  = nodemailer.createTransport({
-    service : "gmail",
-    auth : {
-      user : process.env.MY_GMAIL,
-      pass : process.env.MY_APP_PASSWORD
+    if (!user) {
+      return res.status(404).json({ msg: "User Not Found" });
     }
-  })
 
-  const mailOptions = {
-    from : process.env.MY_GMAIL,
-    to:email,
-    subject:"Reset Your Password",
-    text:`Hello ${email},\n\nClick this link to reset your password:\n${`${reseturl}`}\n\nLink expires in 1 hour`
-  }
+    if (user.authorizedType === "google") {
+      return res.status(400).json({
+        msg: "This account uses Google Sign-In",
+      });
+    }
 
-  await transporter.sendMail(mailOptions)
+    // crypto token
+    const resetToken = crypto.randomBytes(32).toString("hex");
 
-  res.json({
-     msg: `Reset Password link sent to your email`,
-     data : reseturl
+    user.resetPasswordToken = crypto
+      .createHash("sha256")
+      .update(resetToken)
+      .digest("hex");
+
+    user.resetPasswordExpire = Date.now() + 60 * 60 * 1000; // 1hr
+
+    await user.save();
+
+    const reseturl = `${process.env.FRONTEND_URL}/reset-password/${resetToken}`;
+
+    // send Mail
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.MY_GMAIL,
+        pass: process.env.MY_APP_PASSWORD,
+      },
     });
-} catch(err){
-  res.status(500).json({msg : err.message})
-}
+
+    const mailOptions = {
+      from: process.env.MY_GMAIL,
+      to: email,
+      subject: "Reset Your Password",
+      text: `Hello ${email},\n\nClick this link to reset your password:\n${`${reseturl}`}\n\nLink expires in 1 hour`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    res.json({
+      msg: `Reset Password link sent to your email`,
+      data: reseturl,
+    });
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 };
 
 // Reset Password
 
 export const ResetPassword = async (req, res) => {
-  try{
-  const { token } = req.params;
-  const { password } = req.body;
+  try {
+    const { token } = req.params;
+    const { password } = req.body;
 
-  const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
+    const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
 
-  const user = await User.findOne({
-    resetPasswordToken: hashedToken,
-    resetPasswordExpire: { $gt: Date.now() },
-  });
+    const user = await User.findOne({
+      resetPasswordToken: hashedToken,
+      resetPasswordExpire: { $gt: Date.now() },
+    });
 
-  if (!user) {
-    return res.status(404).json({ msg: "Token Invalid or Expired" });
+    if (!user) {
+      return res.status(404).json({ msg: "Token Invalid or Expired" });
+    }
+
+    if (user.authorizedType === "google") {
+      return res.status(400).json({
+        msg: "This account uses Google Sign-In",
+      });
+    }
+
+    user.password = await bcrypt.hash(password, 10);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+
+    res.json({ msg: "Password successfully reserted" });
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
   }
-
-    if(user.authorizedType === "google"){
-   return res.status(400).json({
-      msg: "This account uses Google Sign-In"
-   })
-}
-
-  user.password = await bcrypt.hash(password, 10);
-  user.resetPasswordToken = undefined;
-  user.resetPasswordExpire = undefined;
-
-  await user.save();
-
-  res.json({ msg: "Password successfully reserted" });
-} catch(err){
-  res.status(500).json({msg : err.message})
-}
 };
 
 // Private Response
@@ -386,36 +378,33 @@ export const privateMsg = (req, res) => {
   });
 };
 
-
-
-
 // Login Refresh
 export const Me = async (req, res) => {
   try {
-    const { userId } = req.user   
+    const { userId } = req.user;
 
-    const user = await User.findById(userId).select("-password")
+    const user = await User.findById(userId).select("-password");
 
     if (!user) {
-      return res.status(404).json({ msg: "User not found" })
+      return res.status(404).json({ msg: "User not found" });
     }
 
     res.status(200).json({
-      user : {
-        isLogin : true,
-        roleData : user.role,
-         email: user.email,
+      user: {
+        isLogin: true,
+        roleData: user.role,
+        email: user.email,
         name: user.name,
-        _id : user._id,
+        _id: user._id,
         picture: user.picture,
-        authProvider : req.user.authProvider,
-        loginActivity : Date.now()
-      }
-    })
+        authProvider: req.user.authProvider,
+        loginActivity: Date.now(),
+      },
+    });
   } catch (err) {
-    res.status(500).json({ msg: "Server error" })
+    res.status(500).json({ msg: "Server error" });
   }
-}
+};
 
 // Log Out
 export const Logout = async (req, res) => {
@@ -425,61 +414,59 @@ export const Logout = async (req, res) => {
       signed: true,
       sameSite: "strict",
       maxAge: 15 * 60 * 1000,
-    })
-
-    
+    });
 
     res.clearCookie("refreshToken", {
       httpOnly: true,
       signed: true,
       sameSite: "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
-    })
+    });
 
     res.status(200).json({
       msg: "Logged out successfully",
-      isLogout : true,
-      logoutActivity : Date.now()
-    })
-  } catch (err) {
-    res.status(500).json({ msg: err.message })
-  }
-}
-
-export const Login_Activity = async (req, res) => {
-  try {
-
-    await LoginActivity.findOneAndUpdate(
-      { userId: req.userId },   
-      {
-        userRole: req.role,
-        $push: { loginHistory: new Date() }
-      },
-      {
-        new: true,
-        upsert: true   
-      }
-    );
-
-    res.json({ msg: "Login activity updated" });
-
+      isLogout: true,
+      logoutActivity: Date.now(),
+    });
   } catch (err) {
     res.status(500).json({ msg: err.message });
   }
 };
 
-export const Login_Activty_Get = async(req,res) => {
-  try{
-    const Exist = await LoginActivity.findOne({userId : req.userId})
+export const Login_Activity = async (req, res) => {
+  try {
+    await LoginActivity.findOneAndUpdate(
+      { userId: req.userId },
+      {
+        userRole: req.role,
+        $push: { loginHistory: new Date() },
+      },
+      {
+        new: true,
+        upsert: true,
+      },
+    );
 
-    if(!Exist){
-      return res.status(404).json({msg : "No previous login activity found for this user."})
+    res.json({ msg: "Login activity updated" });
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+};
+
+export const Login_Activty_Get = async (req, res) => {
+  try {
+    const Exist = await LoginActivity.findOne({ userId: req.userId });
+
+    if (!Exist) {
+      return res
+        .status(404)
+        .json({ msg: "No previous login activity found for this user." });
     }
 
-    const loginActivity = await LoginActivity.findOne({userId : req.userId})
+    const loginActivity = await LoginActivity.findOne({ userId: req.userId });
 
-    res.json({msg : 'Login Activity Recieved',data : loginActivity})
-  } catch(err){
-    res.status(500).json({msg : err.message})
+    res.json({ msg: "Login Activity Recieved", data: loginActivity });
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
   }
-}
+};
